dfb9727a6733b1c54f3c713718ce1f02357fab67
BobbyVolpendesta <robert.volpendesta@gmail.com>
2025-07-18 18:16:02 -0500

    Integrate old form data flashing for email on log in screen

--- Http/Forms/LoginForm.php
+++ Http/Forms/LoginForm.php
@@ -6,7 +6,7 @@ use Core\Validator;
 class LoginForm
 {
     protected $errors = [];
-    public function Validate($email, $password)
+    public function validate($email, $password)
     {
         if (!Validator::email($email)) {
             $errors['email'] = 'Please provide a valid email address.';
